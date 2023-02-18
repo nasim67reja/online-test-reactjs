278cc4e593b110f9b0ecb39876eaf1c988b3cea7
Nasim Reja <nasimreja67@gmail.com>
2023-02-18 18:41:22 +0600

    Sorry for being late

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,14 @@
 import { data } from "../public";
 import star from "../assets/star.svg";
+import right from "../assets/right.png";
 
-const Card = ({ el }) => {
+const Card = ({ el, num }) => {
   return (
-    <li className="shadow-product w-[191px] h-[241px] py-4 px-6">
+    <li
+      className={`${
+        num == 3 ? "opacity-40" : ""
+      }shadow-product w-[191px] h-[241px] py-4 px-6`}
+    >
       <div className="flex justify-center items-center">
         <img src={el.picture} alt="" />
       </div>
@@ -19,14 +24,20 @@ const Card = ({ el }) => {
 
 const Slider = () => {
   console.log(data);
-  const upData = data.products.slice(1, 4);
+  const upData = data.products.slice(1, 5);
   return (
-    <div className=" my-12">
-      <ul className="flex gap-10">
-        {upData.map((el, i) => (
-          <Card key={i} el={el} />
-        ))}
-      </ul>
+    <div className=" my-14">
+      <div className="w-fit  relative">
+        <ul className="flex gap-10">
+          {upData.map((el, i) => (
+            <Card key={i} el={el} num={i} />
+          ))}
+        </ul>
+        <div className="absolute right-[40px] top-[45%] shadow-product bg-white rounded-full p-1 cursor-pointer">
+          {" "}
+          <img src={right} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,10 @@ import Right from "../components/Right";
 import Sidebar from "../components/Sidebar";
 import Slider from "../components/Slider";
 
+import Frame16 from "../assets/Frame16.png";
+import Frame17 from "../assets/Frame17.png";
+import Frame23 from "../assets/Frame23.png";
+
 const Home = () => {
   return (
     <>
@@ -18,6 +22,22 @@ const Home = () => {
               <Right />
             </div>
             <Slider />
+            <div className="cus flex gap-4 text-[ #66FF1E] ">
+              <div>Explore The Popular Categories</div>
+              <button>see all</button>
+            </div>
+
+            <div className="flex gap-10 mt-16">
+              <img src={Frame16} alt="" />
+              <img src={Frame17} alt="" />
+            </div>
+            <div className="flex justify-between w-[333px] absolute right-4 top-[55%] px-8">
+              <p>Daily Deals</p>
+              <p>View All</p>
+            </div>
+            <div className="absolute right-0 top-[58%]">
+              <img src={Frame23} alt="" />
+            </div>
           </div>
         </div>
       </div>
